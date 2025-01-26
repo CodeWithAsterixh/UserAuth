988deca7bc130aac6563222088c96c0b672bfc28
p34-pac <peterpaultolulope@gmail.com>
2025-01-26 12:14:17 +0100

    new update

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-import { runValidatorOnElement, toggleCheckBox, toggleClassInItem, toggleDateViewer } from "./js/domUpdates.js"
+import {
+  runValidatorOnElement,
+  toggleCheckBox,
+  toggleClassInItem,
+  toggleDateViewer,
+} from "./js/domUpdates.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  toggleClassInItem("input", "focus");
+  toggleDateViewer();
+  runValidatorOnElement("maininput");
+  // runValidatorOnElement("maininput", "submit")
+  toggleCheckBox();
 
+  // handle submit validator
+  const form = document.getElementById("register_form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    runValidatorOnElement("maininput", "submit")
+    // const formData = new FormData(form);
+    // convert form data to an object
+    // const {
+    //   firstname,
+    //   lastname,
+    //   email_phone,
+    //   dob,
+    //   password,
+    //   confirmPassword,
+    //   remember_me,
+    //   tpp,
+    // } = Object.fromEntries(formData.entries());
 
-document.addEventListener("DOMContentLoaded",()=>{
-    toggleClassInItem("input", "focus")
-    toggleDateViewer()
-    runValidatorOnElement("maininput")
-    toggleCheckBox()
-})
+    
+    
+  });
+});
